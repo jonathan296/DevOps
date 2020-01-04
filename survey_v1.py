0d79a0a6b69b132c681b1d6a3e2efb2eba382eec
jonathan296 <jonathanalvarenga296@gmail.com>
2020-01-04 09:46:37 -0800

    Python Survey Verion 1

---
+++ survey_v1.py
@@ -0,0 +1,16 @@
+print("Welcome to my survey")
+
+first_name = input("First name: ")
+last_name = input("Last name: ")
+date_of_birth = input("Date of birth: ")
+email_add = input("Email: ")
+zip_code = input("Zip: ")
+city = input("City: ")
+state = input("State: ")
+race = input("Race (optional): ")
+age = input("Age: ")
+sex = input("Sex: ")
+
+print(first_name, last_name, date_of_birth,
+      email_add, zip_code, city, state, race, age, sex)
+
